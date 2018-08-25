b48e00aa1fd5b0482deadc8b89e9571172d350b3
Vadims Petrusevs <pvman@inbox.ru>
2018-08-25 10:46:25 +0200

    Menu file added

---
+++ src/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => 'Currencies',
+    'icon' => 'currency',
+    'items' => [
+        '_default' => [
+            'title' => 'Main'
+        ],
+        'settings' => [
+            'title' => 'Settings'
+        ],
+    ]
+];
